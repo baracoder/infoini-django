3e366ee41da2c3ff85ef52dcd7eba7ff39fe10c2
Herman Fries <baracoder@googlemail.com>
2011-05-22 05:13:31 +0200

    news durch generic views. fehlt noch die "Pagination" in Templates

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls.defaults import *
 
+from news.models import News
+from tuer import ist_offen
+from django.views.generic.list_detail import object_list, object_detail
 
-urlpatterns = patterns('news.views',
-	(r'^$', 'index'),
+info_dict = {
+    'queryset': News.objects.all(),
+    'extra_context': {'ist_offen':ist_offen()}
+}
+list_dict = info_dict.copy()
+list_dict['paginate_by']=5
+
+
+
+urlpatterns = patterns('',
+    ( r'^$', object_list, list_dict),
+    ( r'page/(?P<page>[0-9]+)/$', object_list, info_dict),
+    ( r'(?P<object_id>\d+).+/$', object_detail, info_dict),
 )
--- news/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-
-def index(request):
-	    return HttpResponse("Hello, world. You're at the poll index.")
-
-def list(request):
-	latest_news_list = Poll.objects.all().order_by('-pub_date')
