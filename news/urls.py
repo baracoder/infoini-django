2343ab8bd4aa73b085743497ae2d2bf7bb34b9b6
Herman Fries <baracoder@googlemail.com>
2011-05-26 20:34:11 +0200

    fix tuer in urls

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.list_detail import object_list, object_detail
 
 info_dict = {
     'queryset': News.objects.all(),
-    'extra_context': {'ist_offen':ist_offen()}
+    'extra_context': {'ist_offen':ist_offen}
 }
 list_dict = info_dict.copy()
 list_dict['paginate_by']=5
