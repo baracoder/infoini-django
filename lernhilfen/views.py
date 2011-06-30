d976a70775dc0f41bc1070097ee22ce183e8af31
Herman Fries <baracoder@googlemail.com>
2011-06-30 02:06:29 +0200

    templates und views angelegt

--- lernhilfen/views.py
+++ lernhilfen/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+# coding=utf-8
+from django.shortcuts import render_to_response
+from infoini.lernhilfen import  forms
+
+
+def index(request):
+    # übersicht mit filteroptionen anzeigen
+    filterset = LernhilfenFilterSet(request.GET or None)
+    return
+    render_to_response('lernhilfen/index.html', {
+        'filterset':filterset
+        })
+
+def upload(request):
+    # todo
+    return False
+
+def sichten(request):
+    # muss in gruppe FSR oder Helper sein
+    # alle ungesichteten dateinen anzeigen
+    # option zum sichten anbieten
+    pass
+
