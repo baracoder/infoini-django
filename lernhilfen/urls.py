67d0103696bb73318997f34bd562f21f17ff4898
Herman Fries <baracoder@googlemail.com>
2011-07-22 17:55:39 +0200

    url zum Sichten angelegt

--- lernhilfen/urls.py
+++ lernhilfen/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('infoini.lernhilfen.views',
     ( r'^$', 'index'),
     ( r'^upload/$', 'upload'),
+    (r'^sichten/(?P<id>\d+)/(?P<params>.*)$', 'sichten'),
 )
