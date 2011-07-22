36882a48ef9a7027cefce9e4c93ca8fe49f4a4f6
Herman Fries <baracoder@googlemail.com>
2011-07-22 17:56:33 +0200

    index dicht ausgearbeitet, upload vervollständigt, sichten sicht erstellt

--- lernhilfen/views.py
+++ lernhilfen/views.py
@@ -1,34 +1,54 @@
 # coding=utf-8
-from django.shortcuts import render_to_response
 from infoini.lernhilfen import models, forms
 from django.views.decorators.csrf import csrf_protect
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required, permission_required
+from infoini.shortcuts import render_response
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 
-
-
-def index(request):
-    # übersicht mit filteroptionen anzeigen
-    filterset = forms.LernhilfenFilterSet(request.GET or None)
-    return render_to_response('lernhilfen/index.html', {
-        'filter':filterset
-        })
 
 @csrf_protect
+@login_required
+def index(request):
+    if request.GET and request.GET.has_key('semester'):
+        filterset = forms.LernhilfenFilterSet(request.GET)
+    else:
+        filterset = forms.LernhilfenFilterSet()
+
+    l = filterset.qs
+    if request.GET.has_key('ungesichtet'):
+        l = l.filter(gesichtet=False)
+    if not request.user.is_staff:
+        l = l.filter(gesichtet=True)
+
+    c = {
+        'filter':filterset,
+        'l':l,
+        'q':request.GET.urlencode()
+    }
+    c.update(csrf(request))
+    return render_response(request,'lernhilfen/index.html', c)
+
+@csrf_protect
+@login_required
 def upload(request):
     uploadform = forms.LernhilfenUpload(request.POST or None, request.FILES or None)
     if request.POST and uploadform.is_valid():
         uploadform.save()
-
+        return HttpResponseRedirect('/lernhilfen/')
 
     c = {
         'uploadform':uploadform
     }
     c.update(csrf(request))
-    return render_to_response('lernhilfen/upload.html', c)
+    return render_response(request,'lernhilfen/upload.html', c)
 
-def sichten(request):
-    # muss in gruppe FSR oder Helper sein
-    # alle ungesichteten dateinen anzeigen
-    # option zum sichten anbieten
-    pass
+def sichten(request,id,params):
+    if request.user.is_staff:
+        l = models.Lernhilfe.objects.get(pk=id)
+        l.gesichtet = True
+        l.save()
+        return HttpResponseRedirect('/lernhilfen/?'+params)
+    else:
+        return HttpResponseForbidden('Nicht gestattet')
 
