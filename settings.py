bbce2a1050b9cf492b3fa3125dbe04132da1ff35
Herman Fries <baracoder@googlemail.com>
2011-07-23 13:12:27 +0200

    fix: pagination middleware zugefügt

--- settings.py
+++ settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'linaro_django_pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'infoini.urls'
