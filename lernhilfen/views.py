f8a76d1281a7262e68a9bf926eb918f59de4212f
Herman Fries <baracoder@googlemail.com>
2012-04-07 23:25:09 +0200

    lernhilfen_ajax: paginator impelentiert, code aufgeräumt

--- lernhilfen/views.py
+++ lernhilfen/views.py
@@ -24,30 +24,43 @@ def index(request):
     return render_response(request,'lernhilfen/index.html',c)
 
 def ajax_get(request):
+    # anzahl lernhilfen pro seite
+    onpage = 10
+
     if request.GET and request.GET.has_key('semester'):
         filterset = forms.LernhilfenFilterSet(request.GET)
     else:
         filterset = forms.LernhilfenFilterSet()
 
-    l = filterset.qs
-    if request.GET.has_key('ungesichtet'):
-        l = l.filter(gesichtet=False)
-    if not request.user.is_staff:
-        l = l.filter(gesichtet=True)
+    offset = 0
+    if request.GET and request.GET.has_key('offset'):
+        offset = int(request.GET['offset'])
+    list_strt = offset * onpage
+    list_end  = (offset+1) * onpage
 
+    qs = filterset.qs
+    if request.GET.has_key('ungesichtet'):
+        qs = qs.filter(gesichtet=False)
+    if not request.user.is_staff:
+        qs = qs.filter(gesichtet=True)
+
+    qs_page = qs[list_strt:list_end]
+
+    has_prev = offset>0
+    has_next = len(qs)>list_end
 
     fields = [ 'id', 'name', 'endung', 'art', 'modul', 'dozent',
                 'studiengang', 'semester', 'gesichtet']
     rows = []
-    for r in l:
+    for r in qs_page:
         row = {}
         for f in fields:
             row[f]=unicode(r.__getattribute__(f))
             # datei braucht sonder bhandlung
             row['datei'] = unicode(r.datei.url)
         rows.append(row)
-    resp = {'has_prev':False,
-            'has_next':False,
+    resp = {'has_prev':has_prev,
+            'has_next':has_next,
             'rows':rows,
             }
     return HttpResponse(json.dumps(resp), mimetype="application/json")
---
+++ media/lernhilfen.js
@@ -0,0 +1,125 @@
+
+function file_rows(rows) {
+    var file_row_cycle=2;
+    var strr='';
+    for(var i in rows) {
+        strr+=file_row(rows[i],file_row_cycle++%2+1);
+    }
+    $('#filerows').html(strr);
+    //$('#filerows').fadeIn();
+    // TODO: Ajax-loader background ausblenden
+    $('#loader').hide();
+}
+
+function file_row(data,rownr) {
+    var row = '\
+    <div class="filerow filerow'+ rownr +'" id="filerow-'+data['id']+'">\
+        <div class="name" style="padding:2px;text-align:left;">\
+            <a href="'+ data['datei'] +'" target="_blank">'+ data['name'] +'</a>\
+            <sup>('+ data['endung'].substring(1) +')</sup>';
+    if(user_is_staff) {
+        row += '\
+            <div style="float:right;">';
+        if(data['gesichtet']=="False") row += '\
+                <a href="javascript:review('+data['id']+');">\
+                    <img src="/admin/media/img/admin/icon-yes.gif" alt="als gesichtet markieren"/>\
+                </a>';
+        row += '\
+                <a href="/admin/lernhilfen/lernhilfe/'+ data['id'] +'/">\
+                    <img src="/admin/media/img/admin/icon_changelink.gif" alt="Bearbeiten/Löschen" />\
+                </a>\
+            </div>';
+    }
+
+    row +='\
+            <div style="clear:both;"></div>\
+        </div>\
+        <div class="meta">\
+            <span>'+ data['studiengang'] +'</span>\
+            <span>'+ data['modul'] +'</span>\
+            <span>'+ data['dozent'] +'</span>\
+            <span>'+ data['semester'] +'</span>\
+            <span>'+ data['art'] +'</span>\
+            <div style="clear:both;"></div>\
+        </div>\
+    </div>\
+    ';
+    return row;
+}
+
+function get_filter_vals() {
+    filter =  {
+        'studiengang':$('#id_studiengang').val(),
+        'modul':$('#id_modul').val(),
+        'dozent':$('#id_dozent').val(),
+        'semester':$('#id_semester').val(),
+        'art':$('#id_art').val(),
+        'offset':offset,
+    };
+    if(user_is_staff && $('#show_unreviewed').attr('checked')) 
+        filter['ungesichtet']=true;
+    return filter;
+}
+
+function parse_response(response) {
+    if(response['has_next']) $('#btn_nextpage').show();
+    else $('#btn_nextpage').hide();
+
+    if(response['has_prev']) $('#btn_prevpage').show();
+    else $('#btn_prevpage').hide();
+
+    file_rows(response['rows']);
+
+}
+
+
+function request_filerows(l_offset) {
+    if(!l_offset) offset=0;
+    else offset = l_offset;
+    $.get( '/lernhilfen/get/',get_filter_vals(),parse_response );
+    // 2. suchanfrage stellen und anntwort-funktion als callback setzen
+    // 3. ajax-loader bild anzeigen
+    $('#loader').fadeIn();
+    //$('#filerows').hide();
+}
+
+function review(id) {
+    $('#filerow-'+id).css({'background-color':'#080'});
+    $.get( '/lernhilfen/sichten/'+id,review_callback);
+}
+
+function callback_filter_change() {
+    request_filerows();
+}
+
+function review_callback() {
+    request_filerows();
+}
+
+// Optional:
+// Hash-url auswerten
+
+// Ablauf beim laden der seite:
+// 1. Optional: hash auswerten
+// 2. suchanfrage stellen und anntwort-funktion als callback setzen
+// 3. ajax-loader bild anzeigen
+
+request_filerows();
+
+$('.filter select').change(callback_filter_change);
+$('#show_unreviewed').change(callback_filter_change);
+
+$('#btn_hochladen').click( function() {
+    $('#ulform1').fadeToggle(500)
+    $('#ulform2').delay(300).fadeToggle(500)
+    return false;
+}
+);
+
+$('#btn_nextpage').click(function () {
+    request_filerows(offset+1);
+});
+
+$('#btn_prevpage').click(function () {
+    request_filerows(offset-1);
+});
