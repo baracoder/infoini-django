36be4b79559dc3dc7a1c40ee7c9aa411e191e643
Herman Fries <baracoder@googlemail.com>
2012-04-30 20:19:20 +0200

    cafe: /status/ zu html verlinkt

--- cafe_server/urls.py
+++ cafe_server/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import redirect_to
 
 
 urlpatterns = patterns('cafe_server.views',
-    (r'^status/$', 'status_xml'),
+    (r'^status/$', 'status_html'),
     (r'^status.xml$', 'status_xml'),
     (r'^status.html$', 'status_html'),
     (r'^status.json$', 'status_json'),
